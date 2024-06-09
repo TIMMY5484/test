3e93bc9fb330e0ae7e50cd6fccff5ab2e329f204
TIMMY5484 <marshallwgillette@gmail.com>
2024-06-09 18:00:35 -0400

    actually works

--- whateverfilenameyouwant.py
+++ whateverfilenameyouwant.py
@@ -1,19 +1,20 @@
-active=true
+active = "true"
+fileExists = "false"
 print("Welcome to py terminal")
 print("Commands")
 print("file, make a file")
 print("In file, add text to file")
 print("Recall, print file")
-whileactive == true
-enter=input("")
-if enter == ("file")
-filename=input("What is the filename")
-print(filename, "Has been stored")
-elif enter == ("in file")
-fileexists=true
-add=input("What do you want to add to the file")
-elif enter == ("recall")
-if filexists == true
-print(add)
-else 
-print("The file you are looking for dosen't exist")
+while active == "true":
+    enter=input("")
+    if enter == ("file"):
+        filename = input("What is the filename")
+        print(filename, "Has been stored")
+    elif enter == ("in file"):
+        fileExists = "true"
+        add=input("What do you want to add to the file")
+    elif enter == ("recall"):
+        if fileExists == "true":
+            print(add)
+    else:
+        print("The file you are looking for doesn't exist")
