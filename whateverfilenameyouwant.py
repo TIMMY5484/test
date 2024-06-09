e0e9043acd53e7b2d7cdf8abc9dccbe5eec02036
SumgoodPI <bgillettexkcd@gmail.com>
2024-06-09 17:52:51 -0400

    message

--- whateverfilenameyouwant.py
+++ whateverfilenameyouwant.py
@@ -1,1 +1,19 @@
-print("Hello world")
+active=true
+print("Welcome to py terminal")
+print("Commands")
+print("file, make a file")
+print("In file, add text to file")
+print("Recall, print file")
+whileactive == true
+enter=input("")
+if enter == ("file")
+filename=input("What is the filename")
+print(filename, "Has been stored")
+elif enter == ("in file")
+fileexists=true
+add=input("What do you want to add to the file")
+elif enter == ("recall")
+if filexists == true
+print(add)
+else 
+print("The file you are looking for dosen't exist")
